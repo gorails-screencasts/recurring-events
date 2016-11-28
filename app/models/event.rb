14451e82cff40635b0b13ab4dca8956f9a2830df
Chris Oliver <excid3@gmail.com>
2016-11-28 10:49:54 -0600

    First commit

---
+++ app/models/event.rb
@@ -0,0 +1,33 @@
+class Event < ApplicationRecord
+  serialize :recurring, Hash
+
+  def recurring=(value)
+    if RecurringSelect.is_valid_rule?(value)
+      super(RecurringSelect.dirty_hash_to_rule(value).to_hash)
+    else
+      super(nil)
+    end
+  end
+
+  def rule
+    IceCube::Rule.from_hash recurring
+  end
+
+  def schedule(start)
+    schedule = IceCube::Schedule.new(start)
+    schedule.add_recurrence_rule(rule)
+    schedule
+  end
+
+  def calendar_events(start)
+    if recurring.empty?
+      [self]
+    else
+      #start_date = start.beginning_of_month.beginning_of_week
+      end_date = start.end_of_month.end_of_week
+      schedule(start_time).occurrences(end_date).map do |date|
+        Event.new(id: id, name: name, start_time: date)
+      end
+    end
+  end
+end
---
+++ db/migrate/20161114190953_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.datetime :start_time
+      t.text :recurring
+
+      t.timestamps
+    end
+  end
+end
